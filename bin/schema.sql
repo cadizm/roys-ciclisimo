133ab55bb34a44cc7b92432a69b19c54306162da
Michael Cadiz <mcadiz@truecar.com>
2016-12-09 20:21:07 -0800

    local db

--- bin/schema.sql
+++ bin/schema.sql
@@ -1,1 +1,1 @@
-CREATE TABLE images(id standard_resolution varchar(256), standard_resolution varchar(256), caption text);
+CREATE TABLE images(id varchar(256), standard_resolution varchar(256), caption text);
